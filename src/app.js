b2d6392e191a69d1cd5ded1c1ccccdd295cbd843
Roy1473 <pgajdmga@gmail.com>
2020-12-08 17:30:56 +0900

    add  fetch for movie API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
-//export function hello() {
-//  console.log("hello webpack");
-//}
+
+const APIKEY = "eb4e48e8cf76334c1aa24b48aaae72ee";
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=eb4e48e8cf76334c1aa24b48aaae72ee&sort_by=popularity.desc&page=1";
+const IMAGEPATH = "https://image.tmdb.org/t/p/w300";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?api_key=eb4e48e8cf76334c1aa24b48aaae72ee&page=1&include_adult=false&language=ja-JA&query=";
 function App() {
-  return <div>Hello movie</div>;
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(APIURL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data);
+      });
+  }, []);
+  return (
+    <div>
+      {movies.map((movie) => (
+        <Movie />
+      ))}
+    </div>
+  );
 }
+
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,3 +1,4 @@
 import React from "react";
 const Movie = () => <div className="Movie">this is movie component</div>;
+
 export default Movie;
